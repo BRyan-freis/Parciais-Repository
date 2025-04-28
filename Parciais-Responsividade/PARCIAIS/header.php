74d87cab888bbf5ca15fd131885dca33be842e67
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-04-28 12:04:57 -0300

    Add files via upload

---
+++ Parciais-Responsividade/PARCIAIS/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo"><a href=""></a></div>
+        </nav>
+    </header>
+</body>
+
+</html>
