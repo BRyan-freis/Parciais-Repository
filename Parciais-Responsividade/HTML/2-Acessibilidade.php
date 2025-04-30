861e47b0d681156e3e9c3c900bfbac8efb02b5d5
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-04-30 18:44:21 -0300

    Add files via upload

---
+++ Parciais-Responsividade/HTML/2-Acessibilidade.php
@@ -0,0 +1,14 @@
+<title>Acessibilidade Cognitiva</title>
+<link rel="stylesheet" href="../HTML/CSS/styles.css">
+
+<?pHp include '../PARCIAIS/header.php'; ?>
+
+<div class="container">
+    <img src="../IMG/Acessibilidade.png" alt="Img-AC" class="imagem-responsiva">
+    <section class="content">
+        Se quiser, posso criar um diagrama específico com base no tipo de sistema que você está pensando (ex: loja
+        online, sistema escolar, app de delivery etc.). Quer que eu faça um exemplo mais completo baseado em algum caso?
+    </section>
+</div>
+
+<?php include '../PARCIAIS/footer.php'; ?>
---
+++ Parciais-Responsividade/PARCIAIS/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../PARCIAIS/CSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Header</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo"><a href="../HTML/1-index.php"></a>Home</div>
+        <nav>
+            <ul class="menu">
+                <li><a href="../HTML/2-Acessibilidade.php" target="_blank">Acessibilidade Cognitiva para sites</a></li>
+                <li><a href="../HTML/3-Seo.php" target="_blank">SEO e Ranqueamento</a></li>
+                <li><a href="../HTML/4-HospedagemG.php" target="_blank">Hospedagem Gratuita</a></li>
+                <li><a href="../HTML/5-HospedagemP.php" target="_blank">Hospedagem Paga</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- <div class="container">
+        <section class="content">
+            <div>
+            </div>
+        </section>
+    </div> -->
+</body>
+
+</html>
