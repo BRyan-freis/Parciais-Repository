9f0a9f463a161f3c6d40f1b6cef0bbdae4e3a80f
BRyan-freis <bryanferreiradosreisferreira@gmail.com>
2025-04-30 10:39:26 -0300

    update

---
+++ HTML/page4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospedagem Gratuita</title>
+    <link rel="stylesheet" href="../CSS/styles.css">
+</head>
+<body>
+    <div>
+        <h1 class="hospedagem">Hospedagem Gratuita</h1>
+</h1>
+    <div>
+        <a class="site">Sites para hospedagem Gratuita</a>
+</div>
+</body>
+</html>
---
+++ PARCIAIS/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Header</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo"><a href="../HTML/index-home.php" target="_blank"></a>Home</div>
+        <nav>
+            <ul class="menu">
+            <li><a href="../HTML/page1.php" target="_blank">Acessibilidade Cognitiva para sites</a></li>
+            <li><a href="../HTML/page2.php" target="_blank">SEO e Ranqueamento</a></li>
+            <li><a href="../HTML/page3.php" target="_blank">Hospedagem Gratuita</a></li>
+            <li><a href="../HTML/page4.php" target="_blank">Hospedagem Paga</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="content">
+        <div>
+
+            No universo do desenvolvimento web, três tecnologias formam a base de praticamente todos os sites e
+            aplicações modernas: HTML, CSS e JavaScript. Cada uma delas desempenha um papel essencial na criação de
+            páginas web dinâmicas, interativas e esteticamente agradáveis. Juntas, formam a chamada “trindade” do
+            front-end — ou seja, a parte visual com a qual o usuário interage diretamente.
+
+        </div>
+    </section>
+</body>
+
+</html>
