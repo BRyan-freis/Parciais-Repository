b7f1198913edd591db10cbf91dcc1f30f97fb742
Caleb <calebisrs@gmail.com>
2025-05-05 12:02:46 -0300

    Add files via upload

--- Parciais-Responsividade/PARCIAIS/footer.php
+++ Parciais-Responsividade/PARCIAIS/footer.php
@@ -2,12 +2,14 @@
 <footer class="site-footer">
     <div class="footer-container">
         <div class="footer-logo">
-            <a href="../HTML/1-index.php"> MinhaMarca </a>
+            <a href="../HTML/1-index.php"> BlueSite </a>
         </div>
         <div class="footer-links">
-            <a href="/menuefooter-parciais/sobre.php"><i class="fas fa-info-circle"></i>Sobre</a>
-            <a href="/menuefooter-parciais/servicos.php"><i class="fas fa-concierge-bell"></i> Serviços</a>
-            <a href="/menuefooter-parciais/contato.php"><i class="fas fa-envelope"></i> Contato</a>
+            <a href="../HTML/1-index.php"><i class="fas fa-home"></i>Home</a>
+            <a href="../HTML/2-Acessibilidade.php"><i class="fas fa-hand"></i> Acessibilidade</a>
+            <a href="../HTML/3-Seo.php"><i class="fas fa-gear"></i> SEO</a>
+            <a href="../HTML/4-HospedagemG.php"><i class="fas fa-hotel"></i> Hospedagens Gratuitas</a>
+            <a href="../HTML/5-HospedagemP.php"><i class="fas fa-hotel"></i> Hospedagens Pagas</a>
         </div>
 
         <div class="footer-social">
@@ -17,6 +19,6 @@
         </div>
     </div>
     <div class="footer-bottom">
-        <p>&copy; <?php echo date("Y"); ?> MinhaMarca. Todos os direitos reservados.
+        <p>&copy; <?php echo date("Y"); ?> BlueSite. Todos os direitos reservados.
         </p>
     </div>
